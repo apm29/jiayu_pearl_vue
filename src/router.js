2367373764dc86240e50cda0467eef4393d70c41
yingjiawei <yjw999wow@163.com>
2020-04-08 19:36:19 +0800

    dashboard

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,16 +7,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: ()=>import("./App"),
+      children:[
+        {
+          path:"/home",
+          component: ()=>import("./views/Home"),
+        },
+        {
+          path:"/mine",
+          component: ()=>import("./views/About"),
+        }
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,10 @@
 <template>
-  <hello-world />
+  <div>Home</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
   }
 </script>
